aee0999b70548618d780d3d28f71d138e92e8a90
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-06 14:57:19 +0100

    Remove unnecessary includes

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "board.h"
-#include "display_utils.h"
-#include "game.h"
 #include "multiplayer_console_session.h"
-#include "session.h"
 
 using std::cin;
 using std::cout;
