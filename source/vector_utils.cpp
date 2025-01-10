ffc91edd2f42df893bf6a593198777f76fcfd359
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-10 19:58:04 +0100

    Delete vector_utils

--- source/vector_utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "vector_utils.h"
-#include <algorithm>
-
-void vector_utils::add_if_does_not_exist(std::vector<int> &vec, int elem)
-{
-    if(std::find(vec.begin(), vec.end(), elem) != vec.end()) return;
-    vec.push_back(elem);
-}
