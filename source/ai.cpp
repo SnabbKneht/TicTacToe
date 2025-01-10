49f90ca6938118904d1de3e99ead38376a9453f4
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-10 12:29:37 +0100

    Rework get_possible_moves_to_win_immediately function and add get_possible_moves_to_win_in_next_turn function in ai class

--- source/ai.cpp
+++ source/ai.cpp
@@ -1,7 +1,9 @@
 #include "ai.h"
 #include "board.h"
 
-int ai::can_win_in_one_move(const board &brd, symbol self)
+using std::vector;
+
+vector<int> ai::get_possible_moves_to_win_immediately(const board &brd, symbol self)
 {
     const symbol a = brd.get(1);
     const symbol b = brd.get(2);
@@ -13,44 +15,63 @@ int ai::can_win_in_one_move(const board &brd, symbol self)
     const symbol h = brd.get(8);
     const symbol i = brd.get(9);
 
-    return
-        // horizontal 1
-        a == self && b == self && c == symbol::BLANK ||
-        a == symbol::BLANK && b == self && c == self ||
-        a == self && b == symbol::BLANK && c == self ||
+    vector<int> result;
 
-        // horizontal 2
-        d == self && e == self && f == symbol::BLANK ||
-        d == symbol::BLANK && e == self && f == self ||
-        d == self && e == symbol::BLANK && f == self ||
+    // horizontal 1
+    if(a == self && b == self && c == symbol::BLANK) result.push_back(3);
+    else if(a == symbol::BLANK && b == self && c == self) result.push_back(1);
+    else if(a == self && b == symbol::BLANK && c == self) result.push_back(2);
 
-        // horizontal 3
-        g == self && h == self && i == symbol::BLANK ||
-        g == symbol::BLANK && h == self && i == self ||
-        g == self && h == symbol::BLANK && i == self ||
+    // horizontal 2
+    if(d == self && e == self && f == symbol::BLANK) result.push_back(6);
+    else if(d == symbol::BLANK && e == self && f == self) result.push_back(4);
+    else if(d == self && e == symbol::BLANK && f == self) result.push_back(5);
 
-        // vertical 1
-        a == self && d == self && g == symbol::BLANK ||
-        a == symbol::BLANK && d == self && g == self ||
-        a == self && d == symbol::BLANK && g == self ||
+    // horizontal 3
+    if(g == self && h == self && i == symbol::BLANK) result.push_back(9);
+    else if(g == symbol::BLANK && h == self && i == self) result.push_back(7);
+    else if(g == self && h == symbol::BLANK && i == self) result.push_back(8);
 
-        // vertical 2
-        b == self && e == self && h == symbol::BLANK ||
-        b == symbol::BLANK && e == self && h == self ||
-        b == self && e == symbol::BLANK && h == self ||
+    // vertical 1
+    if(a == self && d == self && g == symbol::BLANK) result.push_back(7);
+    else if(a == symbol::BLANK && d == self && g == self) result.push_back(1);
+    else if(a == self && d == symbol::BLANK && g == self) result.push_back(4);
 
-        // vertical 3
-        c == self && f == self && i == symbol::BLANK ||
-        c == symbol::BLANK && f == self && i == self ||
-        c == self && f == symbol::BLANK && i == self ||
+    // vertical 2
+    if(b == self && e == self && h == symbol::BLANK) result.push_back(8);
+    else if(b == symbol::BLANK && e == self && h == self) result.push_back(2);
+    else if(b == self && e == symbol::BLANK && h == self) result.push_back(5);
 
-        // diagonal 1
-        a == self && e == self && i == symbol::BLANK ||
-        a == symbol::BLANK && e == self && i == self ||
-        a == self && e == symbol::BLANK && i == self ||
+    // vertical 3
+    if(c == self && f == self && i == symbol::BLANK) result.push_back(9);
+    else if(c == symbol::BLANK && f == self && i == self) result.push_back(3);
+    else if(c == self && f == symbol::BLANK && i == self) result.push_back(6);
 
-        // diagonal 2
-        c == self && e == self && g == symbol::BLANK ||
-        c == symbol::BLANK && e == self && g == self ||
-        c == self && e == symbol::BLANK && g == self;
+    // diagonal 1
+    if(a == self && e == self && i == symbol::BLANK) result.push_back(9);
+    else if(a == symbol::BLANK && e == self && i == self) result.push_back(1);
+    else if(a == self && e == symbol::BLANK && i == self) result.push_back(5);
+
+    // diagonal 2
+    if(c == self && e == self && g == symbol::BLANK) result.push_back(7);
+    else if(c == symbol::BLANK && e == self && g == self) result.push_back(3);
+    else if(c == self && e == symbol::BLANK && g == self) result.push_back(5);
+
+    return result;
+}
+
+// returns moves that will result in at least two 2/3 complete lines, i.e. the opponent won't be able to block both
+std::vector<int> ai::get_possible_moves_to_win_in_next_turn(const board &brd, symbol self)
+{
+    vector<int> result;
+
+    for(int i = 1; i <= 9; ++i)
+    {
+        if(brd.get(i) != symbol::BLANK) continue;
+
+        board new_brd = brd;
+        new_brd.set(i, self);
+        if(get_possible_moves_to_win_immediately(new_brd, self).size() >= 2)
+            result.push_back(i);
+    }
 }
--- source/ai.h
+++ source/ai.h
@@ -1,12 +1,14 @@
 #ifndef AI_H
 #define AI_H
+#include <vector>
 #include "symbol.h"
 
 class board;
 
 class ai
 {
-    static int can_win_in_one_move(const board &brd, symbol self);
+    static std::vector<int> get_possible_moves_to_win_immediately(const board &brd, symbol self);
+    static std::vector<int> get_possible_moves_to_win_in_next_turn(const board &brd, symbol self);
 };
 
 #endif //AI_H
